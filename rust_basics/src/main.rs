f7760a4c0cbcb215941325e845f6302079cc8a66
Aniket <aniketkumarpaul1@gmail.com>
2023-02-18 10:42:13 +0530

    RUST BASICS - If, For, While, Match

--- rust_basics/src/main.rs
+++ rust_basics/src/main.rs
@@ -64,6 +64,40 @@ fn main() {
     string.push_str("Hi"); // append string
     string = string.replace("Hello", "Hi");
     println!("{}", string);
+
+    // IF
+    let n = 3;
+    if n>0 {
+        println!("greater than 0");
+    } else if n==0 {
+        println!("equal to 0");
+    } else {
+        println!("less than 0");
+    }
+
+    // FOR
+    for i in 0..6 {
+        println!("{}", i); // 0 to 5
+    }
+
+    // WHILE
+    let mut i = 0;
+    while i<4 {
+        println!("{}", i);
+        i+=1;
+    }
+
+    // NOTE:- No need of semicolon for return, break, continue
+
+    // MATCH (switch)
+    let i = 5;
+    match i {
+        0 => println!("0"),
+        1 | 2 => println!("1 or 2"),
+        3..=4 => println!("3 to 4"),
+        _ => println!("Default")
+    }
+
 }
 
 // by default, all functions are private, pub -> public
